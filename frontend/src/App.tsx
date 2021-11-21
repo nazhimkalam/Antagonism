862ad3f6614f157c9aad87f9f3556411b208e6e3
nazhimkalam <nazhimkalam@gmail.com>
2021-11-22 00:24:59 +0530

    cleaned the template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import { Navbar } from './components/Navbar'
-import { About } from './pages/About'
-import { Home } from './pages/Home'
-
-const App: React.FC = () => {
+const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="container">
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
+          {/* <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} /> */}
         </Switch>
       </div>
     </BrowserRouter>
